f343468a05658ed616fdf46bfc897fe46c25c1f6
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-08-15 09:14:34 +0545

    Add common utils

---
+++ src/utils/common.js
@@ -0,0 +1,10 @@
+
+/**
+ * Checks if the provided parameter is a function.
+ *
+ * @param {any} param
+ * @returns {Boolean}
+ */
+export function isFunction(param) {
+  return typeof param === 'function';
+}
